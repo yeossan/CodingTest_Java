87b1f0465976e19a36c6d0820f9b013ae290a2d2
손여산 <94883245+yeossan@users.noreply.github.com>
2024-12-05 11:17:01 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,35 +2,36 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int result = 0;
-        //set 생성
-        HashSet<Integer> reSet = new HashSet<>();
-        HashSet<Integer> lSet = new HashSet<>();
+        int[] student = new int[n + 2];
         
-        for(int i : reserve) {
-            reSet.add(i);
+        for(int r : reserve) {
+            student[r]++;
         }
         
-        for(int i : lost) {
-            if(reSet.contains(i)) {
-                reSet.remove(i); //자기꺼만 보유
-            }
-            else {
-                lSet.add(i);
-            }
+        for(int l : lost) {
+            student[l]--;
         }
         
-        //reserve 기준 앞뒤로 확인 후 빌려주기
-        for(int i : reSet) {
-             if(lSet.contains(i - 1)) {
-                 lSet.remove(i - 1);
-             }
-            else if(lSet.contains(i + 1)) {
-                lSet.remove(i + 1);
+        for(int i = 0; i <= n; i++) {
+            if(student[i] == 1) {
+                if(student[i - 1] == -1) {
+                    student[i]--;
+                    student[i - 1]++;
+                }
+                else if(student[i + 1] == -1) {
+                    student[i]--;
+                    student[i + 1]++;
+                }
             }
         }
         
-        //전체 - lSet
-        return n - lSet.size();
+        for(int i = 1; i <= n; i++) {
+            if(student[i] >= 0) {
+                result++;
+            }
+        }
+        
+        return result;
     }
     
 }
